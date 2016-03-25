a2fa454cacf824bccd664f2c77a3225bec41cdad
Michael Vieth <mavieth@gmail.com>
2016-03-25 16:21:02 -0400

    Added widget removal to install script.

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -6,17 +6,20 @@
 	# Run: . install.sh
 
 
-# ------------------------------------------------------------------------------
 # Set repo location
-sagerepo='https://github.com/mavieth/sage.git'
-wpxfiles="https://github.com/mavieth/wpx.git"
+# ------------------------------------------------------------------------------
+sagerepo='https://github.com/roots/sage.git'
+plugins="https://github.com/mavieth/wpx.git"
 
-localusername=$(whoami)
-sitedir='/Users/'$localusername/'Sites'
-cd $sitedir
+localuser=$(whoami)
+sites=/Users/$localuser/sites
+cd $sites
 
 
-
+if [ ! -d "wpx" ]; then
+	echo "Downloading prerequisite $plugins"
+	git clone $plugins
+fi
 
 echo "Please enter the name of the new wp directory:"
 read  wp_dir
@@ -25,7 +28,7 @@ mkdir $wp_dir
 cd $wp_dir
 wp core download
 
-echo "New Directory Created at: $sitedir/$wp_dir"
+echo "New Directory Created at: $sites/$wp_dir"
 
 # Accepts comma sep. list of pages to create
 echo "Add Pages: "
@@ -33,31 +36,15 @@ read -e allpages
 
 
 
-# # ------------------------------------------------------------------------------
 # # Build new database
-echo "This script will create a local database with a new user. Username & password based on DB name, but this is OK because we're working LOCALLY."
+# # ------------------------------------------------------------------------------
 echo "Enter the name of the new database:"
 read  db_name
-
-# Set up variables
-# Username is just the "salted" db name. For publicly accessible databases, make this more secure.
 dbuser=$db_name
 dbuser+=_user
-# Password is just the "salted" db name. Adjust this for better passwords - require user input.
 newpassword=$db_name
 newpassword+=_pass1234
-
-# # For security purposes, ask for a table prefix. Default to 'wp_'
-# # Do this on the local dev site to ease later uploads
 dbprefix=wp_
-
-# echo "Please enter a database table prefix. Leave out the underscore. Defaults to 'wp_'."
-# read dbprefix
-# dbprefix+="_"
-
-# if [ "$dbprefix" = "" ]; then
-#     $dbprefix=wp_
-# fi
 
 
 echo "Enter the MySQL root password:"
@@ -66,6 +53,10 @@ read  rootpassword
 db="create database $db_name;GRANT ALL PRIVILEGES ON $db_name.* TO $dbuser@localhost IDENTIFIED BY '$newpassword';FLUSH PRIVILEGES;"
 mysql -u root -p$rootpassword -e "$db"
 
+
+
+# # Create Wordpress
+# # ------------------------------------------------------------------------------
 if [ $? != "0" ]; then  # Note: "0" is the return from successful completion of the previous operation in BASH.
 	echo "[Error]: Database creation failed"
 	exit 1
@@ -77,10 +68,13 @@ else
 	echo "Enter WP Username"
 	read wpuser
 
-	wp core install --url=http://localhost/~$localusername/$wp_dir --title="Basic Test Site" --admin_user=$wpuser --admin_password=$wppassword --admin_email=mavieth@gmail.com
+	wp core install --url=http://localhost/~$localuser/$wp_dir --title="Basic Test Site" --admin_user=$wpuser --admin_password=$wppassword --admin_email=mavieth@gmail.com
 fi
 
 
+
+# # Update WordPress Options
+# # ------------------------------------------------------------------------------
 # discourage search engines
 wp option update blog_public 0
 
@@ -89,13 +83,8 @@ wp option update posts_per_page 6
 
 # delete sample page, and create homepage
 wp post delete $(wp post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)
-wp post create --post_type=page --post_title=Home --post_status=publish --post_author=$(wp user get $wpuser --field=ID --format=ids)
+wp post delete 1
 
-# set homepage as front page
-wp option update show_on_front 'page'
-
-# set homepage to be the new page
-wp option update page_on_front $(wp post list --post_type=page --post_status=publish --posts_per_page=1 --pagename=home --field=ID --format=ids)
 
 # create all of the pages
 export IFS=","
@@ -107,100 +96,102 @@ done
 wp rewrite structure '/%postname%/' --hard
 wp rewrite flush --hard
 
-# create a navigation bar
-wp menu create "Main Navigation"
-
 # add pages to navigation
 export IFS=" "
 for pageid in $(wp post list --order="ASC" --orderby="date" --post_type=page --post_status=publish --posts_per_page=-1 --field=ID --format=ids); do
-	wp menu item add-post main-navigation $pageid
+	wp menu item add-post 'primary-navigation' $pageid
 done
 
 # assign navigaiton to primary location
-wp menu location assign main-navigation primary
+wp menu location assign main-navigation primary-navigation
 
 
 setup_theme () {
-	cd $sitedir/$wp_dir/wp-content/themes
+	cd $sites/$wp_dir/wp-content/themes
 	
 	echo "Change Theme Name:"
 	read themename
 	git clone $sagerepo $themename
 
-	git clone $wpxfiles $themename/wpx
-
 	# Change first line of style.css to avoid confusion
-	themeNameUpdate="Theme Name: "
-	themeNameUpdate+=$themename
+	themeline="Theme Name: "
+	themeline+=$themename
 
-	awk -v stylename="$themeNameUpdate" '{ if (NR == 1) print stylename; else print $0}' $sitedir/$wp_dir/wp-content/themes/$themename/style.css > $sitedir/$wp_dir/wp-content/themes/$themename/stylemod.css
-	echo "Enter Sudo Password (optional):"
-	sudo cp $sitedir/$wp_dir/wp-content/themes/$themename/stylemod.css $sitedir/$wp_dir/wp-content/themes/$themename/style.css
-	rm -rf $sitedir/$wp_dir/wp-content/themes/$themename/stylemod.css
+	awk -v stylename="$themeline" '{ if (NR == 1) print stylename; else print $0}' $sites/$wp_dir/wp-content/themes/$themename/style.css > $sites/$wp_dir/wp-content/themes/$themename/stylemod.css
+	echo "Enter Sudo Password:"
+	sudo cp $sites/$wp_dir/wp-content/themes/$themename/stylemod.css $sites/$wp_dir/wp-content/themes/$themename/style.css
+	rm -rf $sites/$wp_dir/wp-content/themes/$themename/stylemod.css
 
 	# Backup original config file
-	cp $sitedir/$wp_dir/wp-config.php $sitedir/$wp_dir/wp-config.bak.php
+	cp $sites/$wp_dir/wp-config.php $sites/$wp_dir/wp-config.bak.php
 
 	# Add a new line
 	newline="/* Define the Roots Environment */\ndefine('WP_ENV', 'development');\n"
 
 	# Insert into a temporary file
-	awk -v insert="$newline" 'NR==3{print insert}1' $sitedir/$wp_dir/wp-config.php > $sitedir/$wp_dir/wp-config-temp.php
+	awk -v insert="$newline" 'NR==3{print insert}1' $sites/$wp_dir/wp-config.php > $sites/$wp_dir/wp-config-temp.php
 
 	# Copy to wp-config.php
-	cp $sitedir/$wp_dir/wp-config-temp.php $sitedir/$wp_dir/wp-config.php
+	cp $sites/$wp_dir/wp-config-temp.php $sites/$wp_dir/wp-config.php
 
 }
 
 # 2
 function install_plugins {
-	# delete akismet and hello dolly
+
+	# go to plugins directory
+	cd $sites/$wp_dir/wp-content/plugins
+
+
+	# Soil
+	git clone https://github.com/roots/soil.git
+	wp plugin activate soil
+
+	# Delete terrible plugins
 	wp plugin delete akismet
 	wp plugin delete hello
 
-	# install wpxfiles
-	wp plugin install $sitedir/wpx/plugins/wp-sync-db.zip --activate
-	wp plugin install $sitedir/wpx/plugins/wp-sync-db-media-files.zip --activate
+	# Deleting sidebar widgets
+	wp widget delete search-2
+	wp widget delete archives-2
+	wp widget delete recent-posts-2
+	wp widget delete recent-comments-2
+	wp widget delete meta-2
+	wp widget delete categories-2
+
+	# install plugins
+	wp plugin install $sites/wpx/plugins/wp-sync-db.zip --activate
+	wp plugin install $sites/wpx/plugins/wp-sync-db-media-files.zip --activate
+
 	wp plugin install advanced-custom-fields --activate
+
 }
 
-# 3
-function install_soil {
-	cd $sitedir/$wp_dir/wp-content/plugins
-	git clone https://github.com/roots/soil.git
-	wp plugin activate soil
-}
 
-# 4
-function setup_node {
-	cd $sitedir/$wp_dir/wp-content/themes/
+function setup_node_and_gulp {
+	cd $sites/$wp_dir/wp-content/themes/
 	wp theme activate $themename
 	cd $themename
-	replace "http://example.dev" "http://localhost/~$localusername/$wp_dir" -- assets/manifest.json
+	# Update URL
+	replace "http://example.dev" "http://localhost/~$localuser/$wp_dir" -- assets/manifest.json
 	npm install
 	bower install
-}
-
-# 5
-function run_gulp {
-	cd $sitedir/$wp_dir/wp-content/themes/$themename
-	subl .
 	gulp
-	gulp watch
+	# Open Sublime
+	subl .
 }
 
-# 6
+
+
 function default_setup {
 	install_plugins
-	install_soil
 	setup_theme
-	setup_node
-	run_gulp
+	setup_node_and_gulp
 }
 
 
 echo "================================================================="
-echo "Installation is complete. Your localusername/password is listed below."
+echo "Installation is complete. Your localuser/password is listed below."
 echo ""
 echo "Username: $wpuser"
 echo "Password: $wppassword"
@@ -212,3 +203,7 @@ read -p "Run default Sage/Gulp/NPM/Bower Install?? [yn]" answer
 if [[ $answer = y ]] ; then
   default_setup
 fi
+ 
+cd $sites/$wp_dir/wp-content/
+
+
